6028d16cb7958fc6a69c388a65326c2b243f588e
Miral <miral.dhankecha127111@marwadiuniversity.ac.in>
2026-03-23 09:32:53 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            rectangle r = new rectangle();
+
+            r.Getdata();
+            r.calculate();
+            r.Display();
+
+            area c = new area();
+
+            c.Getdata1();
+            c.calculate1();
+            c.Display1();
+
+        }
+    }
+}
---
+++ rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_4_1
+{
+    internal class rectangle
+    {
+        int l, w, area;
+
+        public void Getdata()
+        {
+            Console.Write("enter length:  ");
+            l = int.Parse(Console.ReadLine());
+
+            Console.Write("enter widhth:  ");
+            w = int.Parse(Console.ReadLine());
+        }
+
+        public void calculate()
+        {
+            area = l * w;
+
+
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("area:  " + area);
+        }
+    }
+}
